0363162b9f5da8a37fb0de331247e5762abca172
pandarudra <rudrapanda8206@gmail.com>
2025-04-14 09:23:37 +0530

    .

--- backend/src/sockets/appSocket.ts
+++ backend/src/sockets/appSocket.ts
@@ -1,33 +1,51 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 export const onSocket = (io: Server) => {
-  io.on("connection", (socket) => {
+  io.on("connection", (socket: Socket) => {
     console.log("Client connected", socket.id);
 
-    socket.on("join-room", (roomId) => {
+    socket.on("join-room", (roomId: string) => {
       socket.join(roomId);
       const users = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
       io.to(roomId).emit("room-users", users);
     });
 
-    socket.on("leave-room", (roomId) => {
+    socket.on("leave-room", (roomId: string) => {
       socket.leave(roomId);
       const users = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
       io.to(roomId).emit("room-users", users);
     });
 
-    socket.on("file-share", ({ sender, roomId, fileName, fileData }) => {
-      console.log(`File received from ${socket.id} to room ${roomId}`);
-
-      socket.to(roomId).emit("receive-file", {
+    socket.on(
+      "file-share",
+      ({
         sender,
+        roomId,
         fileName,
         fileData,
-      });
-    });
-    socket.on("chat-message", ({ roomId, message }) => {
-      socket.to(roomId).emit("receive-message", `${socket.id}: ${message}`);
-    });
+      }: {
+        sender: string;
+        roomId: string;
+        fileName: string;
+        fileData: ArrayBuffer;
+      }) => {
+        console.log(`File received from ${socket.id} to room ${roomId}`);
+
+        socket.to(roomId).emit("receive-file", {
+          sender,
+          fileName,
+          fileData,
+        });
+      }
+    );
+
+    socket.on(
+      "chat-message",
+      ({ roomId, message }: { roomId: string; message: string }) => {
+        socket.to(roomId).emit("receive-message", `${socket.id}: ${message}`);
+      }
+    );
+
     socket.on("disconnect", () => {
       console.log("Client disconnected", socket.id);
     });
